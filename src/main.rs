4410fe97c1ab5a295f3118a9718b66bf200f82cd
utterwqlnut <dhruva.chayapathy@gmail.com>
2025-07-21 12:32:57 +0530

    added ui layout

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod messages;
 use crate::messages::Messages;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
+    DefaultTerminal, Frame,
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Direction, Layout, Rect},
     style::Stylize,
     symbols::border,
     text::{Line, Text},
     widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
 };
 
 fn main() -> io::Result<()> {
@@ -21,50 +21,61 @@ fn main() -> io::Result<()> {
 
 #[derive(Debug, Default)]
 pub struct Model {
-    counter: i8,
+    counter: u8,
     exit: bool,
 }
 
 impl Widget for &Model {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
+        // Get areas for each region
+        let first_split = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Min(0), Constraint::Length(3)])
+            .split(area);
+
+        let body_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .split(first_split[0]);
+
+        let input_title = Line::from("User Input".bold());
+        let input_block = Block::bordered()
+            .title(input_title.centered())
+            .border_set(border::THICK);
+        let input_area = Rect {
+            x: first_split[1].x + first_split[1].width / 4,
+            y: first_split[1].y,
+            width: first_split[1].width / 2,
+            height: first_split[1].height,
+        };
+
+        let dashboard_area = body_chunks[0];
+        let dash_title = Line::from("Dashboard".bold());
+        let dash_block = Block::bordered()
+            .title(dash_title.centered())
             .border_set(border::THICK);
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
+        let chart_area = body_chunks[1];
+        let chart_title = Line::from("Charts".bold());
+        let chart_block = Block::bordered()
+            .title(chart_title.centered())
+            .border_set(border::THICK);
 
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
+        input_block.render(input_area, buf);
+        dash_block.render(dashboard_area, buf);
+        chart_block.render(chart_area, buf);
     }
 }
 
 impl Model {
-
     // runs the main loop of the app, MUV framework
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             let msg = self.collect_msg()?;
             match msg {
-                Some(msg) => {
-                    self.update(&msg)
-                }
-                None => continue
+                Some(msg) => self.update(&msg),
+                None => continue,
             }
         }
         Ok(())
@@ -72,21 +83,21 @@ impl Model {
 
     // View portion
     fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());    
+        frame.render_widget(self, frame.area());
     }
 
-    fn collect_msg(&mut self) -> io::Result<Option<Messages>>{
+    fn collect_msg(&mut self) -> io::Result<Option<Messages>> {
         match event::read()? {
             // it's important to check that the event is a key press event as
             // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 Ok(self.handle_key_event(key_event))
             }
-            _ => Ok(None)
-        }     
+            _ => Ok(None),
+        }
     }
-    
-    fn handle_key_event(&mut self, key_event: KeyEvent) -> Option<Messages>{
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Option<Messages> {
         match key_event.code {
             KeyCode::Char('q') => Some(Messages::Quit),
             KeyCode::Left => Some(Messages::Decrement),
@@ -96,21 +107,21 @@ impl Model {
     }
 
     // Use different module functions and call setters
-    fn update(&mut self,msg: &Messages) {
+    fn update(&mut self, msg: &Messages) {
         match msg {
             Messages::Increment => self.increment_counter(),
             Messages::Decrement => self.decrement_counter(),
-            Messages::Quit => self.exit()
+            Messages::Quit => self.exit(),
         }
     }
-    
+
     // Replace with a bunch of setters for "Model" state variables
     fn decrement_counter(&mut self) {
-        self.counter-=1;
+        self.counter -= 1;
     }
 
     fn increment_counter(&mut self) {
-        self.counter+=1;
+        self.counter += 1;
     }
 
     fn exit(&mut self) {
--- src/messages.rs
+++ src/messages.rs
@@ -4,3 +4,5 @@ pub enum Messages {
     Decrement,
     Quit,
 }
+
+// Implement parsing of commands
